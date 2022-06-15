16396a1330f32a4d527287401b4cc18faed1867b
Lewis <ljoncb@gmail.com>
2022-06-15 11:43:23 +0100

    Added initial mutations
    The mutations added are for Welsh words. There may be missing values for other languages.

---
+++ src/mutations.ts
@@ -0,0 +1,6 @@
+export interface Mutations {
+  nasal: string
+  init: string
+  aspirate: string
+  soft: string
+}
--- src/word.ts
+++ src/word.ts
@@ -1,9 +1,12 @@
+import { Language } from './language'
+import { Mutations } from './mutations'
 import { WordType } from './word-type'
 
 export interface Word {
   id: number
   normalForm: string
-  lang: string
+  lang: Language
   confirmed: boolean
   type: WordType
+  mutations?: Mutations
 }
